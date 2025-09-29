3e09801de414e082a39d1fa16b1672a62b350d75
Mats Byrkjeland <git@byrkje.land>
2025-09-29 21:56:04 +0200

    Replace deprecated tseslint.config with defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   {
