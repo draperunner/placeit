60d5c0c2618e1b681e3e7880cfb6b3ee19d79a93
Mats Byrkjeland <git@byrkje.land>
2025-09-30 21:41:37 +0200

    Avoid inlining small assets
    To avoid having to allow data: in Content-Security-Policy.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0,
+  },
   server: {
     port: 5423,
   },
