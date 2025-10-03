e15765e65037469e67d7b00efdf07415c01a0cca
Mats Byrkjeland <git@byrkje.land>
2025-10-03 21:18:13 +0200

    Activate jsx-a11y eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,12 @@ import { defineConfig } from "eslint/config";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
+  jsxA11y.flatConfigs.recommended,
   {
     languageOptions: {
       parserOptions: {
@@ -29,6 +31,7 @@ export default defineConfig(
         "error",
         { allowNumber: true },
       ],
+      "jsx-a11y/no-autofocus": "off",
     },
   },
   {
