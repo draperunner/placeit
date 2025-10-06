4ffffa31b8c5cd68f873578e60de097a92b54059
Mats Byrkjeland <git@byrkje.land>
2025-10-06 20:51:48 +0200

    Add some simple feedback when editing profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -18,6 +18,10 @@ export default function Profile() {
   const [email, setEmail] = useState<string>(user?.email || "");
   const [name, setName] = useState<string>(user?.displayName || "");
 
+  const [loading, setLoading] = useState<boolean>(false);
+  const [feedback, setFeedback] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
   const login = () => {
     return navigate("/login");
   };
@@ -36,12 +40,34 @@ export default function Profile() {
   const updateProfileInfo = () => {
     if (!user) return;
 
+    setLoading(true);
+    setFeedback("");
+    setError("");
+
     updateProfile(user, {
       displayName: name,
-    }).catch((error: unknown) => {
-      console.error(error);
-    });
+    })
+      .then(() => {
+        setFeedback(`Profile updated! Your new name is ${name}. Cool!`);
+      })
+      .catch(() => {
+        setError("Something went wrong. Could not update the profile.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFeedback("");
+      setError("");
+    }, 5000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [feedback, error]);
 
   if (!user || user.isAnonymous) {
     return (
@@ -70,16 +96,24 @@ export default function Profile() {
         onChange={(event) => {
           setName(event.target.value);
         }}
+        maxLength={32}
       />
       <Button
         style={{ marginTop: 20, marginRight: 20 }}
         onClick={updateProfileInfo}
+        loading={loading}
       >
         Update profile
       </Button>
       <Button variant="info" style={{ marginTop: 20 }} onClick={logout}>
         Log out
       </Button>
+
+      <div role="alert">
+        {error ? <p style={{ color: "red" }}>{error}</p> : null}
+      </div>
+
+      <div>{feedback ? <p>{feedback}</p> : null}</div>
     </div>
   );
 }
